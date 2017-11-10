892ea82708cd12bf7a7151bf8be73d5da6488982
Kenny <ktran604@hotmail.com>
2017-11-10 15:20:32 -0800

    extras.php file had weird quotes --> ' , broke everything had to change them all

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -1,47 +1,50 @@
 <?php
 /**
- * Custom functions that act independently of the theme templates.
- *
- * @package RED_Starter_Theme
- */
-
+* Custom functions that act independently of the theme templates.
+*
+* @package RED_Starter_Theme
+*/
 /**
- * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- * @return array
- */
+* Adds custom classes to the array of body classes.
+*
+* @param array $classes Classes for the body element.
+* @return array
+*/
 function red_starter_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
-
-	return $classes;
+    // Adds a class of group-blog to blogs with more than 1 published author.
+    if ( is_multi_author() ) {
+        $classes[] = 'group-blog';
+    }
+    
+    return $classes;
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
-
-function inhabient_logo_login() {
-	echo '
-<style type = “text/css”>
-h1 a { background: url(project-04/images/logos/inhabitent-logo-text-dark.svg
-	) !important; }
-</style>';
+//Change WP logo on wp-admin login
+function inhabitant_login_logo(){
+    echo '
+    <style type = "text/css">
+    .login h1 a {
+        background: url(project-04/images/logos/inhabitent-logo-text-dark.svg);
+        height: 56px;
+        width: 314px;
+        background-size: contain;
+    }
+    </style>';
 }
-
-add_action( 'login_head', 'inhabitent_login_logo' );
-
+add_action('login_head', 'inhabitant_login_logo');
 
 
-function my_login_logo_url() {
-	return get_bloginfo( 'http://localhost:8888/inhabitent/' );
+function logo_login_url(){
+    return home_url();
 }
-add_filter( 'login_headerurl', 'my_login_logo_url' );
-
+add_action('login_headerurl', 'logo_login_url');
 
 
 function inhabitent_logo_title(){
-	return 'inhabitent';
+    return 'inhabitent';
 }
-add_filter ( 'login_headertitle', 'inhabitent_logo_url_title');
+add_filter('login_headertitle', 'inhabitent_logo_title');
+
+
+
 
