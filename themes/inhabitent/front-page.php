1ac5dac6401b91643afc90eacbdcc287d6d9d947
Kenny <ktran604@hotmail.com>
2017-11-15 13:24:31 -0800

    added content to front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -17,22 +17,53 @@ get_header(); ?>
 					<h1 class="page-title screen-reader-text"><?php single_post_title();  ?></h1>
 				</header>
 			<?php endif; ?>
+			<div>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/home-hero.jpg" alt="Inhabitent Logo"/>
+			</div>	
+
+
+			<section class="product-info container">
+            <h2>Shop Stuff</h2>
+            <?php
+               $terms = get_terms( array(
+                   'taxonomy' => 'product-type',
+                   'hide_empty' => 0,
+               ) );
+               if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
+            ?>
+               <div class="product-type-blocks">
+
+                  <?php foreach ( $terms as $term ) : ?>
+
+                     <div class="product-type-block-wrapper">
+                        <img src="<?php echo get_template_directory_uri() . '/images/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+                        <p><?php echo $term->description; ?></p>
+                        <p><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
+                     </div>
+
+                  <?php endforeach; ?>
+
+               </div>
+               
+            <?php endif; ?>
+      </section>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			
+			<?php
+			$args = array( 'numberposts' => '3', 'order' => 'ASC');
+			$product_posts = get_posts( $args );
+			$thumbnail = array( 'large' );?>
+				<section class="journal-entries">
+					<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); the_post_thumbnail( 'medium' );
+					the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );?>
+					<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
+					<?php endforeach; wp_reset_postdata(); ?>
+					<?php endif; ?>
+			</section>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
+			
+				
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
