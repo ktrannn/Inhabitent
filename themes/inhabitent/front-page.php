86bad34f692af092cb49249bf427c4888ba86466
Kenny <ktran604@hotmail.com>
2017-11-15 16:44:26 -0800

    fixed footer on frong page some more

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -10,16 +10,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title();  ?></h1>
 				</header>
 			<?php endif; ?>
+
 			<div class="hero-banner"></div>	
+
 			<section class="product-info container">
-            <h2>Shop Stuff</h2>
+
+						<h2>Shop Stuff</h2>
+						
             <?php
                $terms = get_terms( array(
                    'taxonomy' => 'product-type',
@@ -50,18 +54,23 @@ get_header(); ?>
 			$args = array( 'numberposts' => '3', 'order' => 'ASC');
 			$product_posts = get_posts( $args );
 			$thumbnail = array( 'large' );?>
-				<section class="journal-entries">
-					<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); the_post_thumbnail( 'medium' );
-					the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );?>
-					<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
-					<?php endforeach; wp_reset_postdata(); ?>
-					<?php endif; ?>
-			</section>
 
-			
-				
+				<section class="journal-entries">
+
+					<?php foreach ( $product_posts as $post ) : setup_postdata( $post );  ?>
+					
+					<article class="journal-entry">
+					<?php the_post_thumbnail( 'medium' ); ?>
+					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );?>
+					<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
+					</article>
+
+					<?php endforeach; wp_reset_postdata(); ?>
+
+				</section>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
