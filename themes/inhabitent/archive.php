ff0fbd650c59faca6324ff096c221fcbc65975c2
Kenny <ktran604@hotmail.com>
2017-11-18 22:35:39 -0800

    fixed some styling for archive page

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -9,7 +9,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		<?php	echo "<h2>date_format;</h2>"
+		?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
