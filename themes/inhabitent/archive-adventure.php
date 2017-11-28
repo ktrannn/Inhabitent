5230850cf622cba273f2ee0877c6df4e0fe480f4
Kenny <ktran604@hotmail.com>
2017-11-28 10:52:26 -0800

    added single adventures and styled the single adventure page

--- themes/inhabitent/archive-adventure.php
+++ themes/inhabitent/archive-adventure.php
@@ -1,1 +1,40 @@
-archive
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+    <h1 class="page-title">Latest Adventures</h1>
+		<?php if ( have_posts() ) : ?>
+
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				
+			<?php endif; ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content' ); ?>
+			
+				<div class="read-more-wrapper">
+					<a href="<?php the_permalink(); ?>"class="read-more">read more →</a>
+				</div>
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
--- themes/inhabitent/single-adventure.php
+++ themes/inhabitent/single-adventure.php
@@ -1,1 +1,60 @@
-Adventure single
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <header class="entry-header">
+        <?php if ( has_post_thumbnail() ) : ?>
+          <?php the_post_thumbnail( 'large' ); ?>
+        <?php endif; ?>
+    
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    
+        <div class="entry-meta">
+          <?php red_starter_posted_by(); ?>
+        </div><!-- .entry-meta -->
+      </header><!-- .entry-header -->
+    
+      <div class="entry-content">
+        <?php the_content(); ?>
+        <div class="social-buttons">
+          <button type="submit" class="single-product-buttons"><i class="fa fa-facebook" aria-hidden="true"></i>LIKE</button>
+          <button type="submit" class="single-product-buttons"><i class="fa fa-twitter" aria-hidden="true"></i>TWEET</button>
+          <button type="submit" class="single-product-buttons"><i class="fa fa-pinterest" aria-hidden="true"></i>PIN</button>
+        </div>
+      </div><!-- .entry-content -->
+    </article><!-- #post-## -->
+
+			<?php endwhile; ?>
+
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
